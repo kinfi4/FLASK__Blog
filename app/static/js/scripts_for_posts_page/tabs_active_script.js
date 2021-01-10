31d4f417163da8d372669661b7be25a244761fc1
kinfi4 <ilyamakarov223@gmail.com>
2021-01-10 14:25:41 +0200

    active tabs in posts window added

---
+++ app/static/js/scripts_for_posts_page/tabs_active_script.js
@@ -0,0 +1,39 @@
+let tabs;
+let urlParams;
+
+window.onload = function(){
+    tabs = document.getElementsByClassName('filter-posts-title');
+    urlParams = new URLSearchParams(window.location.search);
+
+    deactivate_all_tabs()
+
+    if(urlParams.get('sort_by') == 'world'){
+        tabs[0].classList.add('active');
+    }
+
+    if(urlParams.get('sort_by') == 'country'){
+        tabs[1].classList.add('active');
+    }
+
+    if(urlParams.get('sort_by') == 'following'){
+        tabs[2].classList.add('active');
+    }
+}
+
+function deactivate_all_tabs(){
+    for(let i = 0; i < tabs.length; i++){
+        tabs[i].classList.remove('active');
+    }
+}
+
+document.getElementById('filter-block').onclick = function(event){
+    let target = event.target;
+    for(let i = 0; i < tabs.length; i++){
+        if(tabs[i] == target){
+            deactivate_all_tabs();
+            target.classList.add('active');
+
+            break;
+        }
+    }
+}
