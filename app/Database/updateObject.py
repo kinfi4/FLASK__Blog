540f97a6782274886eacfe373aab6f7af109b569
kinfi4 <ilyamakarov223@gmail.com>
2021-01-25 11:52:07 +0200

    put requests available for post API

--- app/Database/updateObject.py
+++ app/Database/updateObject.py
@@ -2,4 +2,9 @@ from app import db
 
 
 def update_object(cls, id_, **kwargs):
-    pass
+    obj = cls.query.get(id_)
+    for attr, value in kwargs.items():
+        setattr(obj, attr, value)
+
+    db.session.commit()
+    return obj
--- app/REST/get_one_postAPI.py
+++ app/REST/get_one_postAPI.py
@@ -5,6 +5,7 @@ from app import api
 from app.REST.constants import resource_post_fields, request_post_parser
 from app.Database.checkObjectExistence import object_exist
 from app.Database.getCertainObject import get_object_from_db
+from app.Database.updateObject import update_object
 
 from app.models import Post
 
@@ -17,8 +18,13 @@ class OnePostApi(Resource):
 
         return get_object_from_db(Post, id_)
 
+    @marshal_with(resource_post_fields)
+    def put(self, id_):
+        if not object_exist(Post, id_):
+            abort(404)
+
+        args = request_post_parser.parse_args()
+        return update_object(Post, id_, **args)
+
 
 api.add_resource(OnePostApi, '/json_posts/<int:id_>')
-
-
-
